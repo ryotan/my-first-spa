11457d9c0dd3b71bc092377c313afd9f6c00b9c5
Ryo TANAKA <ryotan@ucb-jp.net>
2014-06-15 02:19:14 +0900

    Chapter 3 section 3.6.3.
    * [spa:shell] change URI anchor when chat slider is clicked.

--- app/scripts/spa.shell.js
+++ app/scripts/spa.shell.js
@@ -107,7 +107,11 @@ spa.shell = (function() {
   };
 
   onClickChat = function(event) {
-    toggleChat(stateMap.is_chat_retracted);
+    if(toggleChat(stateMap.is_chat_retracted)) {
+      $.uriAnchor.setAnchor({
+        chat: (stateMap.is_chat_retracted ? 'open' : 'close')
+      });
+    }
     return false;
   };
 
